db64b1dddeb0bfd8fa11f6a51d4ff4c97fb0f54c
Imanalopher <iman.mamadiyev@gmail.com>
2017-10-07 20:31:17 +0600

    CRUD article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,27 @@ class ArticlesController < ApplicationController
   def new
   end
 
-  def create
-    render plain: params[:article].inspect
+  def index
+    @articles = Article.all
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_url
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
